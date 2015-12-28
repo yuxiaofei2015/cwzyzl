13bf231eb09a7cf6865277480f22988062510de3
yuxiaofei <yuxiaofei@youmi.net>
2015-12-28 17:48:40 +0800

    添加了注册页面

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from main import views
 
+app_name = "main"
 urlpatterns = [
-    url(r'^$', views.login, name='login'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^register-result', views.register_result, name='register-result')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def login(request):
-    return HttpResponse("main login")
+    return render(request, 'main/login.html')
+
+
+def register(request):
+    return render(request, 'main/register.html')
+
+
+def register_result(request):
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+    email = request.POST.get("email", "")
+    try:
+        user = User.objects.create_user(username=username, password=password, email=email)
+    except IntegrityError as integrity_error:
+        if integrity_error[0] == 1062:
+            return HttpResponse("Username already exist!")
+    else:
+        return render(request, 'main/register_success.html')
