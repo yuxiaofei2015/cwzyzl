178030de663d58736eddb3cb6643bb487f66d02a
yuxiaofei <yuxiaofei@youmi.net>
2015-12-29 09:59:04 +0800

    添加了数据模型

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User)
+
+
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    createTime = models.DateField()
+    author = models.ForeignKey(User)
+    group = models.ForeignKey(Group)
