38eaff064a51b4bd6dbf2e254a93d9b68cfbcefa
yuxiaofei <yuxiaofei@youmi.net>
2015-12-31 11:21:47 +0800

    添加了退出登录的UI

--- static/blog/blog_custom.js
+++ static/blog/blog_custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#group-manage").click(function(){
-        window.open('http://www.baidu.com');
+    $('#logout').click(function(){
+        window.location.href='/main/logout';
     });
 });
